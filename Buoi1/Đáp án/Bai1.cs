5234b4670f81a55ec627d9c5b18ab5df008dd7ae
tranduc0102 <tranhuynhduc.vn@gmail.com>
2024-11-02 11:46:49 +0700

    Bai2

---
+++ Buoi1/Đáp án/Bai1.cs
@@ -0,0 +1,48 @@
+﻿using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+namespace BaiTapBuoi1
+{
+    internal class Bai1
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8; // Dùng để hiện tiếng việt 
+
+            // # BÀI 1: 
+            // Phương trình dao động điều hòa là: X(t) = A(wt + phi ) 
+            // Trong đó A là biên độ giao động
+            // W là tần số góc
+            // wt + phi là pha dao động tại thời điểm điểm t
+            // phi là pha ban đầu tại thời điểm t
+            //Tần số góc: W = 2*PI*f = 2*PI/T;  // f là tần số dao động, f = 1/T = w/(2*PI) // T là chu kì
+            double A = 5;
+            double W = 2 * Math.PI;
+            double aplha = -2 * Math.PI / 3;
+            double t = 2.4;
+
+            double T = 2 * Math.PI / W; // chu kì của nó là
+            double deltaT = t / T;
+
+            double quangDuong = Math.Floor(deltaT) * 4 * A;
+
+            // Thời gian còn lại trong chu kỳ
+            double thoiGianConLai = deltaT - Math.Floor(deltaT);
+
+            double quangDuongConLai = Math.Floor((thoiGianConLai * 4)) * A; // Làm tròn
+            /*  Console.WriteLine(Math.Floor((thoiGianConLai * 2)));*/
+            thoiGianConLai = (thoiGianConLai * 2 - Math.Floor((thoiGianConLai * 2))) / 2;
+            if (aplha >= 0)
+            {
+                quangDuongConLai = A * Math.Cos(aplha) + A * Math.Cos(W * thoiGianConLai * T - aplha);
+            }
+            else
+            {
+                quangDuongConLai += A - Math.Abs(A * Math.Cos(aplha)) + A - Math.Abs(A * Math.Cos(W * thoiGianConLai * T + aplha));
+            }
+
+            Console.WriteLine($"Quãng đường mà vật đi được là: {quangDuong + quangDuongConLai}");
+
+        }
+    }
+}
---
+++ Buoi1/Đáp án/Bai2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaiTapBuoi1
+{
+    internal class Bai2
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            double Vo = 20;
+            double theTa = 30;
+            double g = 9.8;
+            double Vx;
+            double Vy;
+            //#Bài 2a;
+            Vx = Vo * Math.Cos(theTa * Math.PI / 180);
+            Console.WriteLine($"Vận tốc theo ban đầu theo phương ngang là: {Vx}");
+            Vy = Vo * Math.Sin(theTa * Math.PI / 180);
+            Console.WriteLine($"Vận tốc theo ban đầu theo phương dọc là: {Vy}");
+            //Bài 2b:
+            Console.WriteLine($"Thời gian vật đi lên điểm cao nhất: {Vo * Math.Sin(theTa * Math.PI / 180) / g}");
+            // Bài 2c:
+            Console.WriteLine($"Độ cao cực đại là: {Math.Pow(Vy, 2) / (2 * g)}");
+            //Bài 2d:
+            double tongThoiGianBay = 2 * Vy / g;
+            double quangDuongCoTheDi = Vx * tongThoiGianBay;
+            Console.WriteLine("Quãng đường ngang mà vật đã đi được khi rơi trở lại mặt đất là: " + quangDuongCoTheDi);
+
+
+        }
+    }
+}
